b9890f9c6f64f3bfd003079dd14093983ef40a5d
SiroDamn <balmelli.siro@gmail.com>
2026-04-15 09:39:19 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,6 @@
-{
-  "name": "getwealthy",
-  "private": true,
-  "version": "0.0.1",
-  "type": "module",
-  "scripts": {
-    "dev": "vite",
-    "build": "vite build",
-    "preview": "vite preview"
-  },
-  "dependencies": {
-    "react": "^18.2.0",
-    "react-dom": "^18.2.0"
-  },
-  "devDependencies": {
-    "@vitejs/plugin-react": "^4.2.0",
-    "vite": "^5.0.0"
-  }
-}
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+})
